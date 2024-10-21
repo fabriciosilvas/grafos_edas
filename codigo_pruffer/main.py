5adabcdc3d1df69a7881538d2f1c760cb7f8ed14
fabriciosilvas <112397760+fabriciosilvas@users.noreply.github.com>
2024-10-21 16:48:22 -0300

    Remoção dediferentes tipos de grafo

--- codigo_pruffer/main.py
+++ codigo_pruffer/main.py
@@ -31,26 +31,16 @@ def lerDadosCoodigoPruffer() -> None:
 
     listaAdjacencia: list[list[int]] = [[0] * qtdVertices for _ in range(qtdVertices)]
 
-    print("\nTipos de grafos\n[1] não-direcionado\n[2] direcionado")
-    tipoGrafo: int = int(input("Digite o tipo do grafo: "))
-
     # qtdArestas: int = int(input("\nDigite a quantidade de arestas: "))
 
     print("\nDigite as arestas no formato 'ai aj', sem as aspas e '-1 -1' para finalizar a leitura")
 
-    if tipoGrafo == 1:
-        # for i in range(qtdArestas):
+    # for i in range(qtdArestas):
+    ai, aj = map(int, input().split())
+    while ai != -1 and aj != -1:
+        listaAdjacencia[ai - 1][aj - 1] = 1
+        listaAdjacencia[aj - 1][ai - 1] = 1
         ai, aj = map(int, input().split())
-        while ai != -1 and aj != -1:
-            listaAdjacencia[ai - 1][aj - 1] = 1
-            listaAdjacencia[aj - 1][ai - 1] = 1
-            ai, aj = map(int, input().split())
-    elif tipoGrafo == 2:
-        # for  i in range(qtdArestas):
-        ai, aj = map(int, input().split())
-        while ai != -1 and aj != -1:
-            ai, aj = map(int, input().split())
-            listaAdjacencia[ai - 1][aj - 1] = 1
 
     print(f"Código de Pruffer do grafo: {gerarCodigoPruffer(listaAdjacencia)}")
 
